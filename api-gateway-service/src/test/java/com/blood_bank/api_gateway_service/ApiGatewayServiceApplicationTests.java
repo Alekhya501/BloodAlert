59452142ef26931b056114dc6aa4e4e9f933f777
Alekhya <grandhialekhya57@gmail.com>
2026-05-25 15:54:29 +0530

    Added Files

---
+++ api-gateway-service/src/test/java/com/blood_bank/api_gateway_service/ApiGatewayServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.blood_bank.api_gateway_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ donor-service/src/test/java/com/blood_bank/donor_service/DonorServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.blood_bank.donor_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DonorServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
